a4bf04cc4728482745ce96374d9b7fbd492e907b
SaanikaJ <74892925+SaanikaJ@users.noreply.github.com>
2021-03-04 09:17:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function fact1() {
+    window.alert("This photo signifies hermoine's personality! Fast at any question, even those given by Snape!")
+}
+
+function fact2() {
+    window.alert("Hermoine's typical satisfied smile when she has learnt so much from every book and after her 'light reading' is able to use them in spells!")
+}
